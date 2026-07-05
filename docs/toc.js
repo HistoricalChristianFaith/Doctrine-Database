4324a193a7895daa4d28bf1ab8dce172c6c6582e
Sean Kooyman <github@teachthe.net>
2026-07-05 12:37:29 -0400

    astrology section

--- docs/toc.js
+++ docs/toc.js
@@ -34,7 +34,8 @@
     "infant-baptism": "Infant Baptism",
     "church-buildings": "The Place of Christian Worship",
     "perpetual-virginity": "The Perpetual Virginity of Mary",
-    "writing-on-the-ground": "What Jesus Wrote on the Ground"
+    "writing-on-the-ground": "What Jesus Wrote on the Ground",
+    "astrology": "Astrology and the Stars"
   };
 
   // The page's depth-relative path prefix ("", "../", "../../", "../../../"), read from its
